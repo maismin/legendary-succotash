008c8ea258626a4727429007095e8a8df4cdf5c6
Simon Mai <maismin@gmail.com>
2021-04-15 15:16:43 -0700

    Refactor Current User Route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('hi');
-});
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };

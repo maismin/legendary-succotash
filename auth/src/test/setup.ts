338c8fb4ecf89fac64a586402e904be04d69ac40
Simon Mai <maismin@gmail.com>
2021-04-15 20:44:06 -0700

    Add Global Function To Get Cookie For Testing

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,16 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      getCookie(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -28,3 +38,18 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.getCookie = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  return response.get('Set-Cookie');
+};

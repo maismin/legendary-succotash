8fe9b0b6397a29080bde473e5b6bd98e600b5339
Simon Mai <maismin@gmail.com>
2021-04-15 20:13:30 -0700

    Add Default JWT_KEY For Testing

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,8 @@ import { app } from '../app';
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'super_secret_key_for_jwt';
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 

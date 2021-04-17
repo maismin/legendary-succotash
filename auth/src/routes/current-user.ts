c7338a0737f49e3adc36900aca257c53f5551946
Simon Mai <maismin@gmail.com>
2021-04-16 20:05:45 -0700

    Refactor Auth Service
    Moved error and middleware directory to a NPM module and imported that
    module

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
-
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@ticketingsm/common';
 
 const router = express.Router();
 

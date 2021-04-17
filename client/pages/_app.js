f4e0a5b5b9e6517710ac5b8c3e3950345de831b5
Simon Mai <maismin@gmail.com>
2021-04-16 18:45:35 -0700

    Add Header Component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,11 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <h1>Header! {currentUser.email} </h1>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </div>
   );

a85f27d55a3e5349b4a996e1356898ef7baa5944
Simon Mai <maismin@gmail.com>
2021-04-18 15:44:23 -0700

    Add Version Field To Order Model

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@ticketingsm/common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 import { TicketDoc } from './ticket';
 
@@ -15,6 +16,7 @@ interface OrderDoc extends mongoose.Document {
   status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
+  version: number;
 }
 
 interface OrderModel extends mongoose.Model<OrderDoc> {
@@ -42,10 +44,12 @@ const orderSchema = new mongoose.Schema(
         ret.id = ret._id;
         delete ret._id;
       },
-      versionKey: false,
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);

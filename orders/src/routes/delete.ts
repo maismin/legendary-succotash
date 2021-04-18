7ce82ac4182ee73b8a09baae33842dd9f855e682
Simon Mai <maismin@gmail.com>
2021-04-18 12:35:42 -0700

    Update Delete Order Route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,6 +6,8 @@ import {
 } from '@ticketingsm/common';
 
 import { Order, OrderStatus } from '../models/order';
+import { natsWrapper } from '../nats-wrapper';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
 
 const router = express.Router();
 
@@ -15,7 +17,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -28,7 +30,12 @@ router.delete(
     order.status = OrderStatus.Cancelled;
     await order.save();
 
-    // TODO: publish an event saying this order was cancelled
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }

ac79bba3ae759567fb5a85f6fd5f7893efe54173
Simon Mai <maismin@gmail.com>
2021-04-17 19:57:13 -0700

    Add Empty Routes For Orders

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,11 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@ticketingsm/common';
 
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
+
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -14,6 +19,11 @@ app.use(
   })
 );
 app.use(currentUser);
+
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();

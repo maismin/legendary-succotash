f9341452c6a2a9d0ec4fc4a7e8b605fc1fcc2a52
Simon Mai <maismin@gmail.com>
2021-04-17 22:08:22 -0700

    Add Route And Test To Cancel Orders

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,10 +1,37 @@
 import express, { Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@ticketingsm/common';
+
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
 router.delete(
   '/api/orders/:orderId',
-  async (req: Request, res: Response) => {}
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // TODO: publish an event saying this order was cancelled
+
+    res.status(204).send(order);
+  }
 );
 
 export { router as deleteOrderRouter };
